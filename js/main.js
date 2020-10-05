1b8f88f5e9b5736b9b5c123aa06cd48bf860862f
belalex26 <alex.belova26@gmail.com>
2020-10-05 18:10:23 +0300

    массив с данными

--- js/main.js
+++ js/main.js
@@ -1,12 +1,89 @@
 'use strict';
 
-//Напишите функцию для создания массива из 8 сгенерированных JS объектов. Каждый объект массива ‐ описание похожего объявления неподалёку. Структура объектов должна быть следующей:
-//У блока .map уберите класс .map--faded
-//На основе данных, созданных в первом пункте, создайте DOM-элементы, соответствующие меткам на карте, и заполните их данными из массива. Итоговую разметку метки .map__pin можно взять из шаблона #pin
+let getRandomData = function () {
+  const TYPES = ["palace", "flat", "house", "bungalow"];
+  const TIMES = ["12:00", "13:00", "14:00"];
+  const PHOTOS= ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+  const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+  let priceMax = 30000;
+  let roomMax = 6;
+  let guestsMax = 10;
+  let advertising = [];
+  let countAds = 8;
 
-const ads = [
+  //случайное число
+  let getRandomNumber = function (max, offset = 0) {
+    return Math.floor((Math.random() * Math.floor(max)) + offset);
+  };
 
-];
+   //случайный текст
+  let getRandomText = function (max) {
+    let text = "";
+    let possibleValue = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+    for (let i = 0; i < max; i++) {
+      text += possibleValue.charAt(getRandomNumber(possibleValue.length));
+    }
+    return text;
+  };
+
+  //cлучайный массив
+  let getRandomArrayValue = function () {
+    return arr[getRandomNumber(arr.length)];
+  };
+
+  //координаты
+  let getRandomCoordinateX = function () {
+    let containerWidth = document.querySelector(".map").clientWidth;
+    return getRandomNumber(containerWidth + 1);
+  };
+
+  let getRandomCoordinateY = function () {
+    const COORDINATE_Y_RANGE = 500;
+    const COORDINATE_Y_OFFSET = 130;
+
+    return getRandomNumber(COORDINATE_Y_RANGE, COORDINATE_Y_OFFSET);
+  };
+
+  //наполнение объектами
+  for (i = 0; i <= countAds; i++) {
+    let item = {
+      "author": {
+        "avatar": "img/avatars/user0${i}.png"
+      },
+    "offer": {
+        "title": getRandomText (8),
+        "address": "${getRandomCoordinateX()}, ${getRandomCoordinateY()}",
+        "price":  getRandomNumber(priceMax),
+        "type": getRandomArrayValue(TYPES),
+        "rooms": getRandomNumber(roomMax),
+        "guests": getRandomNumber(guestsMax),
+        "checkin": getRandomArrayValue(TIMES),
+        "checkout": getRandomArrayValue(TIMES),
+        "features": getRandomLengthArrayValues(FEATURES),
+        "description": getRandomText (280),
+        "photos": getRandomLengthArrayValues(PHOTOS),
+    },
+    "location": {
+        "x": getRandomCoordinateX(),
+        "y": getRandomCoordinateY()
+    }
+  }
+    advertising.push(item);
+  };
+
+return advertising;
+};
+
+
+
+
+
+
+
+
+
+
 
 
 var mapActive = document.querySelector(".map");
