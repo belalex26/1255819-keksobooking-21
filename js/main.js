4559dd21183ce9ea58072b6fe010a3c73921689d
belalex26 <alex.belova26@gmail.com>
2020-10-08 17:34:22 +0300

    метки на карте

--- js/main.js
+++ js/main.js
@@ -1,90 +1,13 @@
 'use strict';
-/*
-let getRandomData = function () {
+
   const TYPES = ["palace", "flat", "house", "bungalow"];
   const TIMES = ["12:00", "13:00", "14:00"];
   const PHOTOS= ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
   const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-  let priceMax = 30000;
-  let roomMax = 6;
-  let guestsMax = 10;
-  const advertising = [];
-  let countAds = 8;
-
-  //случайное число
-  let getRandomNumber = function (max, offset = 0) {
-    return Math.floor((Math.random() * Math.floor(max)) + offset);
-  };
-
-   //случайный текст
-  let getRandomText = function (max) {
-    let text = "";
-    let possibleValue = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-
-    for (let i = 0; i < max; i++) {
-      text += possibleValue.charAt(getRandomNumber(possibleValue.length));
-    }
-    return text;
-  };
-
-  //cлучайный массив
-  let getRandomArrayValue = function (arr) {
-    return arr[getRandomNumber(arr.length)];
-  };
-
-  let getRandomLengthArrayValues = function (arr) {
-    return arr.filter(() => {
-      return getRandomNumber(2);
-    });
-  };
-
-  //координаты
-  let getRandomCoordinateX = function () {
-    let containerWidth = document.querySelector(".map").clientWidth;
-    return getRandomNumber(containerWidth + 1);
-  };
-
-  let getRandomCoordinateY = function () {
-    const COORDINATE_Y_RANGE = 500;
-    const COORDINATE_Y_OFFSET = 130;
-    return getRandomNumber(COORDINATE_Y_RANGE, COORDINATE_Y_OFFSET);
-  };
-
-  //наполнение объектами
-  for (i = 0; i <= countAds; i++) {
-    let item = {
-      "author": {
-        "avatar": "img/avatars/user0${i}.png"
-      },
-    "offer": {
-        "title": getRandomText (8),
-        "address": "${getRandomCoordinateX()}, ${getRandomCoordinateY()}",
-        "price":  getRandomNumber(priceMax),
-        "type": getRandomArrayValue(TYPES),
-        "rooms": getRandomNumber(roomMax),
-        "guests": getRandomNumber(guestsMax),
-        "checkin": getRandomArrayValue(TIMES),
-        "checkout": getRandomArrayValue(TIMES),
-        "features": getRandomLengthArrayValues(FEATURES),
-        "description": getRandomText (280),
-        "photos": getRandomLengthArrayValues(PHOTOS),
-    },
-    "location": {
-        "x": getRandomCoordinateX(),
-        "y": getRandomCoordinateY()
-    }
-  }
-    advertising.push(item);
-  };
-
-return advertising;
-
-};
-
-console.log (advertising);
-
-*/
-
+  const priceMax = 10000;
+  const roomMax = 6;
+  const guestsMax = 10;
+  const countAds = 8;
 
 let getRandomNumber = function (max, offset = 0) {
   return Math.floor((Math.random() * Math.floor(max)) + offset);
@@ -102,8 +25,35 @@ let getRandomCoordinateY = function () {
   return getRandomNumber(COORDINATE_Y_RANGE, COORDINATE_Y_OFFSET);
 };
 
-(function(){
-  let countAds = 8;
+let getRandomArrayValue = function (arr) {
+  return arr[getRandomNumber(arr.length)];
+};
+
+/*
+let getNewArr = function (arr) {
+  let newArr = arr.slice (0, arr.length - 1);
+  return newArr;
+};
+
+function shuffle(newArr) {
+  for (let i = newArr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+  return newArr;
+}
+//тестирование
+shuffle(PHOTOS);
+console.log (PHOTOS);
+*/
+
+let getRandomLengthArrayValues = function (arr) {
+  return arr.filter(() => {
+    return getRandomNumber(2);
+  });
+};
+
+let getAds = function () {
   let ads = new Array();
   for (var i = 1; i <= countAds; i++) {
     let item = {
@@ -112,19 +62,49 @@ let getRandomCoordinateY = function () {
       },
       "offer": {
         "title": `ads`,
-        "address": "${getRandomCoordinateX()}, ${getRandomCoordinateY()}",
-
-
-      }
+        "address": `${getRandomCoordinateX()}, ${getRandomCoordinateY()}`,
+        "price":  getRandomNumber(priceMax),
+        "type": getRandomArrayValue(TYPES),
+        "rooms": getRandomNumber(roomMax),
+        "guests": getRandomNumber(guestsMax),
+        "checkin": getRandomArrayValue(TIMES),
+        "checkout": getRandomArrayValue(TIMES),
+        "features": getRandomLengthArrayValues(FEATURES),
+        "description": `description`,
+        "photos": getRandomLengthArrayValues(PHOTOS),
+    },
+    "location": {
+        "x": getRandomCoordinateX(),
+        "y": getRandomCoordinateY()
     }
+  }
     ads.push(item);
   }
-  console.log (ads);
-}) ();
+}
 
+const adDataMock = getAds();
 
-(function(){
+const getMapActive = function () {
   let mapActive = document.querySelector(".map");
   mapActive.classList.remove("map--faded");
-}) ();
+}
+getMapActive ();
+
+//метка
+const mapPinsBlock = document.querySelector(".map__pins");
+let createPins = function () {
+  const pinsFragment = document.createDocumentFragment();
+  const pinTemplate = document.querySelector("#pin").content.querySelector("button");
+
+  for (let i = 1; i < countAds; i++) {
+    const pin = pinTemplate.cloneNode(true);
+    pin.style = `left:` + `${getRandomCoordinateX()}` + `px; top:` +`${getRandomCoordinateY()}` + `px;`;
+    pin.querySelector(`img`).src = `img/avatars/user0${i}.png`;
+    pin.querySelector(`img`).alt = `ads`;
+    pinsFragment.appendChild(pin);
+  }
+
+  mapPinsBlock.appendChild(pinsFragment);
+}
+createPins(adDataMock);
 
