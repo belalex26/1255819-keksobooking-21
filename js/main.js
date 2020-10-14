82b0f1f7bad33e8c619f63c89d94f25472346dde
belalex26 <alex.belova26@gmail.com>
2020-10-14 15:39:40 +0300

    валидация формы

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ const guestsMax = 10;
 const countAds = 8;
 const containerWidth = document.querySelector(".map").clientWidth;
 const mainPin = document.querySelector(`.map__pin--main`);
+const mainForm = document.querySelector(`.ad-form`);
+const addressData = document.querySelector(`#address`);
+const PIN_HEIGHT = 20;
 
 const getRandomNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
@@ -61,10 +64,8 @@ let getAds = function () {
   }
   return ads;
 }
-
 //
 const adDataMock = getAds();
-
 
 const getMapActive = function () {
   let mapActive = document.querySelector(".map");
@@ -87,7 +88,6 @@ const createPins = function (ads) {
   }
   mapPinsBlock.appendChild(pinsFragment);
 };
-//createPins(adDataMock);
 
 // отключение формы
 
@@ -97,7 +97,14 @@ const formTurnOff = function () {
     fieldsets[i].disabled = true;
   }
 }
-formTurnOff ();
+
+//адрес
+const getAddress = function (offset) {
+  let top = Number(mainPin.style.top.slice(0, 3));
+  let left = mainPin.style.left.slice(0, 3);
+  let adress = top + offset + ` / ` + left;
+  addressData.value = adress;
+}
 
 //активация карты
 const formTurnOn = function formTurnOn () {
@@ -107,16 +114,107 @@ const formTurnOn = function formTurnOn () {
   }
 }
 
+const mouseClick = function () {
+  mainPin.addEventListener('mousedown', function (e) {
+    if (e.button === 0) {
+      getActivePages();
+      getAddress(PIN_HEIGHT);
+    }
+  })
+};
+
+const mouseClickRemove = function () {
+  mainPin.removeEventListener('mousedown', function (e) {})
+}
+
+const buttonClick = function () {
+  mainPin.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      getActivePages();
+      getAddress(PIN_HEIGHT);
+    }
+  })
+};
+
+const buttonClickRemove = function () {
+  mainPin.removeEventListener('keydown', function (e) {})
+}
+
+const getDisablePages = function () {
+  getAddress (0);
+  formTurnOff ();
+  mouseClick ();
+  buttonClick ();
+}
+
+getDisablePages();
+
 const getActivePages = function () {
+  mainForm.classList.remove("ad-form--disabled");
+  mouseClickRemove ();
+  buttonClickRemove ();
   getMapActive();
   formTurnOn();
   createPins(adDataMock);
 }
 
-mainPin.addEventListener('mousedown', function () {
-  getActivePages();
-});
+const propertyType = document.querySelector(`#type`);
+const propertyPrice = document.querySelector(`#price`);
 
-mainPin.addEventListener(`keydown`, function (e) {
-  getActivePages();
-});
+const selectPrice = function () {
+  if (propertyType.value === `house`) {
+    propertyPrice.setAttribute(`min`, `1000`);
+    propertyPrice.setAttribute(`placeholder`, `1000`);
+  } else if (propertyType.value === `bungalow`) {
+      propertyPrice.setAttribute(`min`, `3000`);
+      propertyPrice.setAttribute(`placeholder`, `3000`);
+    } else if (propertyType.value === `flat`) {
+        propertyPrice.setAttribute(`min`, `5000`);
+        propertyPrice.setAttribute(`placeholder`, `5000`);
+      } else if (propertyType.value === `palace`) {
+          propertyPrice.setAttribute(`min`, `10000`);
+          propertyPrice.setAttribute(`placeholder`, `10000`);
+        }
+}
+
+propertyType.addEventListener(`change`, selectPrice);
+
+const RoomGuestRation = {
+  1: [1],
+  2: [1, 2],
+  3: [1, 2, 3],
+  100: [0]
+};
+
+const roomNumberSelect = document.querySelector(`#room_number`);
+const capacitySelect = document.querySelector(`#capacity`);
+const submitBtn = document.querySelector(`.ad-form__submit`);
+
+const checkPlaceValidity = function () {
+  const roomGuests = RoomGuestRation[roomNumberSelect.value];
+  const message = roomGuests.indexOf(+capacitySelect.value) === -1 ? `Это количество гостей сюда не поместится` : ``;
+  capacitySelect.setCustomValidity(message);
+  };
+
+const onSubmitBtnClick = function () {
+  checkPlaceValidity();
+};
+
+const disableСapacityOptions = function (inputValue) {
+  const capacityOptions = capacitySelect.querySelectorAll(`option`);
+  capacityOptions.forEach(function (it) {
+    it.disabled = true;
+  });
+  RoomGuestRation[inputValue].forEach(function (it) {
+    capacitySelect.querySelector(`option` + '[value="' + it + '"]').disabled = false;
+    capacitySelect.value = it;
+  });
+};
+
+const onRoomNumberSelectChange = function (evt) {
+  evt.target.setCustomValidity(``);
+  disableСapacityOptions(roomNumberSelect.value);
+};
+
+roomNumberSelect.addEventListener(`change`, onRoomNumberSelectChange);
+submitBtn.addEventListener(`click`, onSubmitBtnClick);
